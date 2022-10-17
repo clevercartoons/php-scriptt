77610093c61c7acc2d892067c643ee63be6256b7
clevercartoons <92776705+clevercartoons@users.noreply.github.com>
2022-10-17 16:19:13 +0200

    1
    1

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="row justify-content-center border-top-e6 mt-4 ps-4 pt-4">
+    <p><em>A Blue Box made with</em> <i class="bi bi-heart"></i>. A proud product of Mane Brothers - LATITUDE</a></p>
+</div>
